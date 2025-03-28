15fd5e724e2b4b7c399f0f31b9e7f273e20b6d75
seungah <162815560+ssssaaaaas@users.noreply.github.com>
2025-03-28 15:27:00 +0900

    2nd

---
+++ 1st_hw_ChoiSeungAh/1st_hw_ChoiSeungAh/_st_hw_ChoiSeungAhApp.swift
@@ -0,0 +1,17 @@
+//
+//  _st_hw_ChoiSeungAhApp.swift
+//  1st_hw_ChoiSeungAh
+//
+//  Created by 최승아 on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct _st_hw_ChoiSeungAhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 1st_hw_ChoiSeungAh/1st_hw_ChoiSeungAhTests/_st_hw_ChoiSeungAhTests.swift
@@ -0,0 +1,17 @@
+//
+//  _st_hw_ChoiSeungAhTests.swift
+//  1st_hw_ChoiSeungAhTests
+//
+//  Created by 최승아 on 3/18/25.
+//
+
+import Testing
+@testable import _st_hw_ChoiSeungAh
+
+struct _st_hw_ChoiSeungAhTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
